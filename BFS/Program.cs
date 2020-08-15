c3fe427af58a7e7a7936d236e0bbb29023c47f49
BEST-Thanawat <thanawat.sukwibul@gmail.com>
2020-08-15 16:17:53 -0700

    Add project files.

---
+++ BFS/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace BFS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Breadth-First Search");
+            Node root = new Node(1);
+            root.Left = new Node(2);
+            root.Right = new Node(3);
+            root.Left.Left = new Node(4);
+            root.Left.Right = new Node(5);
+            root.Right.Left = new Node(6);
+
+            BFS(root);
+        }
+
+        static void BFS(Node root)
+        {
+            if (root == null) return;
+
+            Queue<Node> queue = new Queue<Node>();
+            queue.Enqueue(root);
+
+            while(queue.Count > 0)
+            {
+                Node currentNode = queue.Peek();
+                Console.WriteLine(currentNode.Data);
+
+                if (currentNode.Left != null)
+                {
+                    queue.Enqueue(currentNode.Left);
+                }
+
+                if(currentNode.Right != null)
+                {
+                    queue.Enqueue(currentNode.Right);
+                }
+
+                queue.Dequeue();
+            }
+        }
+    }
+
+    public class Node
+    {
+        public int Data { get; set; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+
+        public Node(int data)
+        {
+            Data = data;
+        }
+    }
+}
